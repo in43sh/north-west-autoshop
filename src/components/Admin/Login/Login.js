2571091fb88077fd893cfee2e6eff89d404108ac
Sergey Sherstobitov <in43sh@gmail.com>
2018-05-26 03:06:30 -0700

    started working on input validation

--- src/components/Admin/Login/Login.js
+++ src/components/Admin/Login/Login.js
@@ -9,9 +9,13 @@ class Login extends Component {
   constructor () {
     super();
     this.state = {
-      userInput: '',
-      passInput: '',
-      errorMessage: ''
+      email: '',
+      password: '',
+      errorMessage: '',
+      formErrors: {email: '', password: ''},
+      emailValid: false,
+      passwordValid: false,
+      formValid: false
     }
   }
 
@@ -32,12 +36,51 @@ class Login extends Component {
       }
     }
   }
+  
+  handleUserInput (e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({[name]: value}, 
+                  () => { this.validateField(name, value) });
+    console.log(this.state.email);
+  }
+
+  validateField(fieldName, value) {
+    let fieldValidationErrors = this.state.formErrors;
+    let emailValid = this.state.emailValid;
+    let passwordValid = this.state.passwordValid;
+  
+    switch(fieldName) {
+      case 'email':
+        emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+        fieldValidationErrors.email = emailValid ? '' : ' is invalid';
+        break;
+      case 'password':
+        passwordValid = value.length >= 6;
+        fieldValidationErrors.password = passwordValid ? '': ' is too short';
+        break;
+      default:
+        break;
+    }
+    this.setState({formErrors: fieldValidationErrors,
+                    emailValid: emailValid,
+                    passwordValid: passwordValid
+                  }, this.validateForm);
+  }
+  
+  validateForm() {
+    this.setState({formValid: this.state.emailValid && this.state.passwordValid});
+  }
+
+  errorClass(error) {
+    return(error.length === 0 ? '' : 'has-error');
+  }
 
   login = () => {
-    const username = this.state.userInput
-    const password = this.state.passInput
+    console.log('hui');
+    const {email, password} = this.state
     axios.post('/login', {
-      username,
+      email,
       password
     }).then(response => {
       console.log('response ->', response)
@@ -63,14 +106,14 @@ class Login extends Component {
       <div className="login-main">
           <div className="login-input-div">
              <div className="input-data"> {/* check this class */}
-              <input className="input-data-field" placeholder="username" ref="username" onChange={ (e) => this.setState({ userInput: e.target.value }) } />
+              <input className="input-data-field" placeholder="username" ref="username" onChange={(event) => this.handleUserInput(event)} />
             </div>
 
             <div className="input-data">{/* check this class */}
-              <input className="input-data-field"  placeholder="password" type="password" ref="password" onChange={ (e) => this.setState({ passInput: e.target.value }) } />
+              <input className="input-data-field"  placeholder="password" type="password" ref="password" onChange={(event) => this.handleUserInput(event)} />
             </div>
 
-            <button className="submit-btn" onClick={ this.login }>sign in</button>
+            <button className="submit-btn"  disabled={!this.state.formValid} onClick={ this.login }>sign in</button>
             <div className="login-info-container">{this.state.errorMessage}</div>
           </div>
       </div>
