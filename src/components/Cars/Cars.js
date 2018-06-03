0918b605831a4c2bfd794cb36472e8bb2b8bd201
BazT <bazetug@gmail.com>
2018-06-03 00:20:03 -0700

    6/3 empty cars and parts messages; design fix

--- src/components/Cars/Cars.js
+++ src/components/Cars/Cars.js
@@ -22,65 +22,40 @@ export default class Cars extends Component {
   }
 
   render() {
-    const listOfCars = this.state.cars.map((car, index) => {
-      return (
-        <Link to={`/car/${ car._id }`} key={index}>
-          <div id="box">
-            <div className="container" id="car">
-              <div className="row">
-                <div className="col-md-12" id="top" >
-                  <h1 id="title">{car.year} {car.brand} {car.model}</h1>
+    var listOfCars
+    if(this.state.cars.length>0){
+      listOfCars = this.state.cars.map((car, index) => {
+        return (
+          <Link to={`/car/${ car._id }`} key={index}>
+            <div id="box">
+              <div className="container" id="car">
+                <div className="row">
+                  <div className="col-md-12" id="top" >
+                    <h1 id="title">{car.year} {car.brand} {car.model}</h1>
+                  </div>
                 </div>
-              </div>
-              <div className="row">
-                <div className="col-md-4">
-                  {car.photos.length === 0 && <img src={aws} alt="part" id="photo" className="photos"/>} 
-                  {car.photos.length>0 && <img src={car.photos[0]} alt="part" id="photo" className="photos"/>}
-                </div>
-                <div className="col-md-4">
-                  <p id="model">Mileage: {car.mileage} Color: {car.color}</p>
-                  <p id="condition">{car.description}</p>
-                </div>
-                <div className="col-md-4">
-                  <p id="price">${car.price}</p>
+                <div className="row">
+                  <div className="col-md-4">
+                    {car.photos.length === 0 && <img src={aws} alt="part" id="photo" className="photos"/>} 
+                    {car.photos.length>0 && <img src={car.photos[0]} alt="part" id="photo" className="photos"/>}
+                  </div>
+                  <div className="col-md-4">
+                    <p id="model">Mileage: {car.mileage} Color: {car.color}</p>
+                    <p id="condition">{car.description}</p>
+                  </div>
+                  <div className="col-md-4">
+                    <p id="price">${car.price}</p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </Link>
-        
-        // <div class="col-md-3 car-cont" key={index}>
-        //   <figure class="card card-product">
-        //     <div class="img-wrap"> 
-        //       <img src={aws} alt="part" id="photo" />
-        //       <Link to={`/car/${ car._id }`}><i class="fa fa-search-plus"></i> Quick view</Link>
-        //     </div>
-        //     <figcaption class="info-wrap">
-        //       <h6 class="title text-dots"><a href="#">{car.year} {car.brand} {car.model}</a></h6>
-        //       <div class="action-wrap">
-        //         <div class="price-wrap h5">
-        //           <span> Mileage: {car.mileage}</span>
-        //           <span class="price-new"> ${car.price}</span>
-        //         </div> 
-        //       </div> 
-        //     </figcaption>
-        //   </figure> 
-        // </div> 
-
-
-        // <div class="col-md-3 car-cont" key={index}>
-        //    <figure class="card card-product">
-        //       <div class="producttitle"><h3>{car.year} {car.brand} {car.model}</h3></div>
-
-        //       {!car.photo && <img src={aws} class="img-responsive" />}
-        //       {car.photo && <img src={car.photo[0]} class="img-responsive" />}
-        //       <div class="productprice"><div class="pull-right"><Link to={`/car/${ car._id }`}><button class="btn btn-danger btn-sm" role="button">View</button></Link></div><div class="pricetext">${car.price}</div></div>
-        //     </figure> 
-        // </div>
-        
-        
-
-      )})
+          </Link>
+        )})
+    }else{
+      listOfCars = ``
+      
+    }
+    
 
     return (
       <div id="body_list_cars">
@@ -90,6 +65,14 @@ export default class Cars extends Component {
               <div id="main2">
                 { listOfCars }
               </div>
+          {this.state.cars.length < 1 && <div class="row text-center margin-b-40 emptyCars">
+               <div class="col-sm-6 col-sm-offset-3 emptyCars-box">
+                  <h1>Sorry, we have nothing to sell at the moment</h1>
+                  <h4>Please, come back later to check new offers!</h4>
+                </div>
+              </div>
+                
+                }
           </div>
       </div>
     );
